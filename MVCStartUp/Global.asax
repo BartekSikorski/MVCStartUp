1515fdee78beb7488483b2800a384a909971c7b0
Bartek <bartek.sikorski@gmail.com>
2016-01-29 08:15:34 +0100

    New one

---
+++ MVCStartUp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCStartUp.MvcApplication" Language="C#" %>
---
+++ MVCStartUp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCStartUp.Startup))]
+namespace MVCStartUp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
